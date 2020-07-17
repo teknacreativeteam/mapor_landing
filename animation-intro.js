ff97b2599ed2caf2d79be50cdcc837d67fb78c3f
teknacreative <hq@teknacreative.com>
2020-07-17 15:57:45 +0200

    animation working

--- animation-intro.js
+++ animation-intro.js
@@ -5,3 +5,22 @@ const tl = new TimelineMax ();
 
 tl.fromTo(brush, 2, {top: "0px"}, {top:"500px"},)
 .fromTo(paint, 2, {height: "0px"}, {height:"600px"}, "-=2");
+
+var controller = new ScrollMagic.Controller();
+var tldois = new TimelineMax();
+
+tldois.fromTo(brush, 2, {top: "500px"}, {top:"1000px"},)
+.fromTo(paint, 2, {height: "600px"}, {height:"1200px"}, "-=2");
+
+var scene = new ScrollMagic.Scene({
+  triggerElement: "#trigone",
+  triggerHook: 0
+})
+  .addIndicators({
+    colorTrigger: "white",
+    colorStart: "white",
+    colorEnd: "white",
+    indent: 5
+  })
+  .setTween(tldois)
+  .addTo(controller);
