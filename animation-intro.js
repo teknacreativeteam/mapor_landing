98e3d414d37fc289ad2261310b2dbef04af267d0
teknacreative <hq@teknacreative.com>
2020-07-21 17:46:38 +0200

    another iteration

--- animation-intro.js
+++ animation-intro.js
@@ -6,21 +6,42 @@ const tl = new TimelineMax ();
 tl.fromTo(brush, 2, {top: "0px"}, {top:"500px"},)
 .fromTo(paint, 2, {height: "0px"}, {height:"600px"}, "-=2");
 
-var controller = new ScrollMagic.Controller();
+var controllerdois = new ScrollMagic.Controller();
 var tldois = new TimelineMax();
 
 tldois.fromTo(brush, 2, {top: "500px"}, {top:"1000px"},)
 .fromTo(paint, 2, {height: "600px"}, {height:"1200px"}, "-=2");
 
 var scene = new ScrollMagic.Scene({
-  triggerElement: "#trigone",
-  triggerHook: 0
-})
-  .addIndicators({
-    colorTrigger: "white",
-    colorStart: "white",
-    colorEnd: "white",
-    indent: 5
+    triggerElement: "#trigone",
+    triggerHook: 0
   })
-  .setTween(tldois)
-  .addTo(controller);
+    .addIndicators({
+      colorTrigger: "white",
+      colorStart: "white",
+      colorEnd: "white",
+      indent: 5
+    })
+    .setTween(tldois)
+    .addTo(controllerdois);
+
+
+var controllertres = new ScrollMagic.Controller();
+var tltres = new TimelineMax();
+
+tltres.fromTo(brush, 2, {top: "1000px"}, {top:"1500px"},)
+.fromTo(paint, 2, {height: "1200px"}, {height:"1700px"}, "-=2");
+
+var scenetres = new ScrollMagic.Scene({
+    triggerElement: ".gallery-section",
+    triggerHook: 0
+  })
+    .addIndicators({
+      colorTrigger: "lime",
+      colorStart: "lime",
+      colorEnd: "lime",
+      indent: 5
+    })
+    .setTween(tltres)
+    .addTo(controllertres);
+
